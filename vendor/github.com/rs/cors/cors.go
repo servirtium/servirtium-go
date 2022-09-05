4d2fe1629d4491b3c2893df3ee693380f43b3bb9
Duong Pham <tanphamhaiduong@gmail.com>
2022-09-05 12:43:44 +0700

    chore: update module name and remove vendor files

--- vendor/github.com/rs/cors/cors.go
+++ vendor/github.com/rs/cors/cors.go
@@ -41,7 +41,7 @@ type Options struct {
 	// as argument and returns true if allowed or false otherwise. If this option is
 	// set, the content of AllowedOrigins is ignored.
 	AllowOriginFunc func(origin string) bool
-	// AllowOriginRequestFunc is a custom function to validate the origin. It takes the HTTP Request object and the origin as
+	// AllowOriginFunc is a custom function to validate the origin. It takes the HTTP Request object and the origin as
 	// argument and returns true if allowed or false otherwise. If this option is set, the content of `AllowedOrigins`
 	// and `AllowOriginFunc` is ignored.
 	AllowOriginRequestFunc func(r *http.Request, origin string) bool
@@ -65,9 +65,6 @@ type Options struct {
 	// OptionsPassthrough instructs preflight to let other potential next handlers to
 	// process the OPTIONS method. Turn this on if your application handles OPTIONS.
 	OptionsPassthrough bool
-	// Provides a status code to use for successful OPTIONS requests.
-	// Default value is http.StatusNoContent (204).
-	OptionsSuccessStatus int
 	// Debugging flag adds additional output to debug server side CORS issues
 	Debug bool
 }
@@ -100,10 +97,8 @@ type Cors struct {
 	allowedOriginsAll bool
 	// Set to true when allowed headers contains a "*"
 	allowedHeadersAll bool
-	// Status code to use for successful OPTIONS requests
-	optionsSuccessStatus int
-	allowCredentials     bool
-	optionPassthrough    bool
+	allowCredentials  bool
+	optionPassthrough bool
 }
 
 // New creates a new Cors handler with the provided options.
@@ -176,13 +171,6 @@ func New(options Options) *Cors {
 		c.allowedMethods = convert(options.AllowedMethods, strings.ToUpper)
 	}
 
-	// Options Success Status Code
-	if options.OptionsSuccessStatus == 0 {
-		c.optionsSuccessStatus = http.StatusNoContent
-	} else {
-		c.optionsSuccessStatus = options.OptionsSuccessStatus
-	}
-
 	return c
 }
 
@@ -223,7 +211,7 @@ func (c *Cors) Handler(h http.Handler) http.Handler {
 			if c.optionPassthrough {
 				h.ServeHTTP(w, r)
 			} else {
-				w.WriteHeader(c.optionsSuccessStatus)
+				w.WriteHeader(http.StatusOK)
 			}
 		} else {
 			c.logf("Handler: Actual request")
@@ -238,8 +226,6 @@ func (c *Cors) HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
 		c.logf("HandlerFunc: Preflight request")
 		c.handlePreflight(w, r)
-
-		w.WriteHeader(c.optionsSuccessStatus)
 	} else {
 		c.logf("HandlerFunc: Actual request")
 		c.handleActualRequest(w, r)
@@ -258,7 +244,7 @@ func (c *Cors) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.Handl
 		if c.optionPassthrough {
 			next(w, r)
 		} else {
-			w.WriteHeader(c.optionsSuccessStatus)
+			w.WriteHeader(http.StatusOK)
 		}
 	} else {
 		c.logf("ServeHTTP: Actual request")
@@ -371,12 +357,6 @@ func (c *Cors) logf(format string, a ...interface{}) {
 	}
 }
 
-// check the Origin of a request. No origin at all is also allowed.
-func (c *Cors) OriginAllowed(r *http.Request) bool {
-	origin := r.Header.Get("Origin")
-	return c.isOriginAllowed(r, origin)
-}
-
 // isOriginAllowed checks if a given origin is allowed to perform cross-domain requests
 // on the endpoint
 func (c *Cors) isOriginAllowed(r *http.Request, origin string) bool {
@@ -404,7 +384,7 @@ func (c *Cors) isOriginAllowed(r *http.Request, origin string) bool {
 }
 
 // isMethodAllowed checks if a given method can be used as part of a cross-domain request
-// on the endpoint
+// on the endpoing
 func (c *Cors) isMethodAllowed(method string) bool {
 	if len(c.allowedMethods) == 0 {
 		// If no method allowed, always return false, even for preflight request
@@ -435,7 +415,6 @@ func (c *Cors) areHeadersAllowed(requestedHeaders []string) bool {
 		for _, h := range c.allowedHeaders {
 			if h == header {
 				found = true
-				break
 			}
 		}
 		if !found {
